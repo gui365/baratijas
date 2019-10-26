ed7435a139c8e632cfdebe7002910f779c313acf
Guillermo Barila <gui365@yahoo.com.ar>
2019-10-26 09:43:03 -0400

    Basic layout finished. Adding participants to list.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Button } from 'react-bootstrap';
+
+import SideNav from './components/sideNav/SideNav';
+import MainContent from './components/mainContent/MainContent';
+import Header from './components/header/Header';
+
+class App extends React.Component {
+
+  state = {
+    vuelta: 1,
+    participantes: [],
+    premios: [],
+    nowPlaying: false
+  }
+
+  agregarParticipante = (nombre) => {
+    let exists = false;
+    for (let i = 0; i < this.state.participantes.length; i++) {
+      if(this.state.participantes[i] === nombre) {
+        exists = true;
+      }
+    }
+
+    if(nombre !== '' && !exists) {
+      const newArray = [...this.state.participantes];
+      newArray.push(nombre);
+      this.setState({
+        participantes: newArray
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Row>
+          <SideNav
+            participantes={this.state.participantes}
+            agregar={this.agregarParticipante}
+          />
+          { this.state.nowPlaying
+            ? <MainContent />
+            : <Button
+                  size='lg'
+                  className='mx-auto btn-comenzar'
+                  style={{ alignSelf: 'center' }}>
+                Comenzar Juego
+              </Button>
+          } 
+        </Row>
+      </>
+    );
+  }
 }
 
 export default App;
